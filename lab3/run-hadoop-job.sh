3eea8e5822add702e81200a6c746b0a897fcfabf
artemiyjjj <artemiyromanoff@gmail.com>
2026-02-15 00:28:46 +0300

    Add parameter parsing for starting script

--- lab3/run-hadoop-job.sh
+++ lab3/run-hadoop-job.sh
@@ -4,10 +4,59 @@ set -ex
 
 EXEC_TIMESTAMP="$(date +%d/%m/%Y/%H/%M/%S)"
 
-export WORKER_NODES="${1:-1}"
-METRIC="${2:-sum}"
-INPUT_DIR="${3:-/input}"
-OUTPUT_DIR="${4:-/output/${EXEC_TIMESTAMP}}"
+INPUT_DIR="/input"
+OUTPUT_DIR="/output/${EXEC_TIMESTAMP}"
+WORKER_NODES="1"
+METRIC="sum"
+LINESPLIT_NUM="10000"
+REDUCERS_NUM="1"
+
+get_help() {
+	echo "Usage: $0 [-e <env>] [-d]"
+	exit 0
+}
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -i|--input)
+      INPUT_DIR="$2"
+      shift
+      shift
+      ;;
+	  -o|--output)
+      OUTPUT_DIR="$2"
+      shift
+      shift
+      ;;
+    -w|--workers)
+      WORKER_NODES="$2"
+      shift
+      shift
+      ;;
+    -m|--metric)
+      METRIC="$2"
+      shift
+      shift
+      ;;
+    -l|--linesplit)
+      LINESPLIT_NUM="$2"
+      shift
+      shift
+      ;;
+    -r|--reducers)
+      REDUCERS_NUM="$2"
+      shift
+      shift
+      ;;
+    -h|--help)
+      get_help
+      ;;
+    *)
+      echo "Invalid option: $1" >&2
+      exit 1
+      ;;
+  esac
+done
 
 VERSION=1.0.0
 JAR_NAME="lab3-hadoop-${VERSION}-all.jar"
@@ -32,8 +81,9 @@ while [ "$IS_PUT" == "false" ]; do
 	docker exec namenode bash -c "hadoop fs -put -f /data/*.csv /input/" && IS_PUT=true || sleep 3
 done
 
-echo "Running MapReduce job..."
-docker exec namenode bash -c "hadoop jar /jars/${JAR_NAME} ${INPUT_DIR} ${OUTPUT_DIR} ${METRIC}"
+echo "Running MapReduce job"
+docker exec namenode bash -c "hadoop jar /jars/${JAR_NAME} ${INPUT_DIR} ${OUTPUT_DIR} ${METRIC}" \
+	-Dmapred.reduce.tasks=${REDUCERS_NUM} -Dmapreduce.input.lineinputformat.linespermap=${LINESPLIT_NUM}
 
 mkdir -p ./results
 echo "Job completed. Results:"
